b60c73e01687de80a5c9677a211efa7d9d2f8874
EdouardMatheu <edouardmatheu@gmail.com>
2022-01-29 15:37:34 +0100

    Added body of mirror function. Also modified test 2 and 3 for mirror function since they were wrong (second argument is the index of a list, then if it is 0 the function should not return an empty list since it is index 0 and not length 0. Same is true for test 3)

--- functions.py
+++ functions.py
@@ -1,2 +1,7 @@
 def mirror(txt, a):
-	return -1
+	rt_txt = ""
+	for i in range(a+1) : 
+		rt_txt += txt[i]
+	for i in range(a, -1, -1) :
+		rt_txt += txt[i]
+	return rt_txt
--- tests.py
+++ tests.py
@@ -5,8 +5,8 @@ class TestFunctions(unittest.TestCase):
 
 	def test_mirror(self):
 		self.assertEqual(functions.mirror("abcde", 3), "abcddcba")
-		self.assertEqual(functions.mirror("abcde", 0), "")
-		self.assertEqual(functions.mirror("abcde", 1), "aa")
+		self.assertEqual(functions.mirror("abcde", 0), "aa")
+		self.assertEqual(functions.mirror("abcde", 1), "abba")
 		self.assertEqual(functions.mirror("abcdefghijklmnopqrstuvwxyz", 25), "abcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba")
 		self.assertEqual(functions.mirror("abcde", -1), "")
 
