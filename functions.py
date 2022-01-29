0795ba83984c0196f507463692557a362d01ab89
EdouardMatheu <edouardmatheu@gmail.com>
2022-01-29 16:52:26 +0100

    Added body of deriv2 function. Also modified wrong tests that had too many elements in return list

--- functions.py
+++ functions.py
@@ -20,4 +20,17 @@ def deriv(l):
 	return rt_l
 
 def deriv2(l):
-	return -1
+	interval = 1.0
+	temp_l = []
+	rt_l = []
+	if len(l) < 3:
+		return None
+	for e in l :
+		if type(e) != float:
+			return None
+	for i in range(len(l)-1):
+		temp_l.append((l[i+1]-l[i])/interval)
+	for i in range(len(temp_l)-1):
+		rt_l.append((temp_l[i+1]-temp_l[i])/interval)
+
+	return rt_l
